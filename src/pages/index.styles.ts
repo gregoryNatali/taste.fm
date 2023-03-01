846c0b137f7b887594797b7c6ccd6c23db22c99c
gregory natali <gregory.natali4@gmail.com>
2023-03-01 11:56:31 -0300

    create home page with inputs

---
+++ src/pages/index.styles.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`
+
+export const InputsContainer = styled.div`
+display: flex;
+flex-direction: column;
+width: 10vw;
+
+div {
+  display: flex;
+  flex-direction: column;
+  margin: 0.5rem;
+  gap: 0.3rem;
+}
+`
